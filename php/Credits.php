0127a9139814fb7853a7c1ab9ccf1ca2526830c6
kandreev-contact <68382841+kandreev-contact@users.noreply.github.com>
2020-10-12 17:52:00 +0200

    tabla hecha

--- php/Credits.php
+++ php/Credits.php
@@ -1,17 +1,40 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <?php include '../html/Head.html'?>
+	<?php include '../html/Head.html'?>
 </head>
+
 <body>
-  <?php include '../php/Menus.php' ?>
-  <section class="main" id="s1">
-    <div>
-
-      <h2>DATOS DEL AUTOR/AUTORES</h2>
-
-    </div>
-  </section>
-  <?php include '../html/Footer.html' ?>
+	<?php include '../php/Menus.php' ?>
+	<section class="main" id="s1">
+		<div>
+			<table border="1px">
+				<tr>
+					<th colspan="2" width="700">
+						<h2>DATOS DEL AUTOR/AUTORES</h2>
+					</th>
+				</tr>
+				<tr>
+					<td width="50%">Konstantin Todorov Andreev</td>
+					<td width="50%">Daniel Ruskov Vangelov</td>
+				</tr>
+				<tr>
+					<td>Ingereria de Software</td>
+					<td>Ingenieria de Computadores</td>
+				</tr>
+				<tr>
+					<td>imagen</td>
+					<td>IMG</td>
+				</tr>
+				<tr>
+					<td>imagen</td>
+					<td>druskov001@ikasle.ehu.es</td>
+				</tr>
+			</table>
+		</div>
+	</section>
+	<?php include '../html/Footer.html' ?>
 </body>
+
 </html>
