c4188bcfe88b643ff4eb7368655fdfaf89781241
kandreev-contact <68382841+kandreev-contact@users.noreply.github.com>
2020-10-15 16:25:46 +0200

    php conexion realizada

--- php/AddQuestion.php
+++ php/AddQuestion.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <?php include '../html/Head.html'?>
+  <?php include '../html/Head.html' ?>
 </head>
+
 <body>
   <?php include '../php/Menus.php' ?>
   <section class="main" id="s1">
     <div>
-
-			Código PHP para añadir una pregunta sin imagen
-
+      Código PHP para añadir una pregunta sin imagen
+      <?php
+      // Realizar conexion php
+      $mysqli = mysqli_connect("localhost", "root", "", "quiz");
+      if (!$mysqli) {
+        die("Fallo al conectar a MySQL: " . mysqli_connect_error());
+      }
+      echo 'Connection OK';
+      // Cerrar conexión
+      mysqli_close($mysqli);
+      ?>
     </div>
   </section>
   <?php include '../html/Footer.html' ?>
 </body>
+
 </html>
