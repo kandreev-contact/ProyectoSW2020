73e91c92de6c1228037a2e1995ad9f66d8db9233
dikata1106 <danielruskov1998@gmail.com>
2020-10-25 13:35:29 +0100

    Retoque error de imagen grande y si no se incluye
    Daba error por la llamada de una funcion en caso de no incluir imagen o si fuera grande la imagen. Solucionado. Posible futura mejora en ShowQuestionsWithImage.php para que no muestre nada si no hay imagen relacionada en vez del texto alternativo a una imagen

--- php/AddQuestionWithImage.php
+++ php/AddQuestionWithImage.php
@@ -16,12 +16,16 @@
         if (!$mysqli) {
           die("Fallo al conectar a MySQL: " . mysqli_connect_error());
         }
-        // Operar
         // echo "Connection OK.";
+        // Operar con la BD
         $imagen = $_FILES['file']['tmp_name'];
-        $imagen_b64 = base64_encode(file_get_contents($imagen));
-        $sql = "INSERT INTO preguntas(email, enunciado, respuestac, respuestai1, respuestai2, respuestai3, complejidad, tema, imagen) VALUES('$_REQUEST[Direccion_de_correo]', '$_REQUEST[Pregunta]', '$_REQUEST[Respuesta_correcta]', '$_REQUEST[Respuesta_incorrecta_1]', '$_REQUEST[Respuesta_incorrecta_2]', '$_REQUEST[Respuesta_incorrecta_3]', '$_REQUEST[complejidad]', '$_REQUEST[tema]', '$imagen_b64');";
-        if(!mysqli_query($mysqli, $sql)) {
+        if ($imagen != "" ) {
+          $imagen_b64 = base64_encode(file_get_contents($imagen));
+          $sql = "INSERT INTO preguntas(email, enunciado, respuestac, respuestai1, respuestai2, respuestai3, complejidad, tema, imagen) VALUES('$_REQUEST[Direccion_de_correo]', '$_REQUEST[Pregunta]', '$_REQUEST[Respuesta_correcta]', '$_REQUEST[Respuesta_incorrecta_1]', '$_REQUEST[Respuesta_incorrecta_2]', '$_REQUEST[Respuesta_incorrecta_3]', '$_REQUEST[complejidad]', '$_REQUEST[tema]', '$imagen_b64');";
+        } else {
+          $sql = "INSERT INTO preguntas(email, enunciado, respuestac, respuestai1, respuestai2, respuestai3, complejidad, tema) VALUES('$_REQUEST[Direccion_de_correo]', '$_REQUEST[Pregunta]', '$_REQUEST[Respuesta_correcta]', '$_REQUEST[Respuesta_incorrecta_1]', '$_REQUEST[Respuesta_incorrecta_2]', '$_REQUEST[Respuesta_incorrecta_3]', '$_REQUEST[complejidad]', '$_REQUEST[tema]');";
+        }
+          if(!mysqli_query($mysqli, $sql)) {
           die("Fallo al insertar en la BD: " . mysqli_error($mysqli));
         }
         echo "Pregunta guardada en la BD";
